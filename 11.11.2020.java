d42a6283378dd8c47eb3c6d8f397f0b739d85d7e
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 11:47:38 +0300

    Create 11.11.2020.java

---
+++ 11.11.2020.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class sdg {
+    public class Program{
+
+        public static void main(String[] args) {
+
+            Person tom;
+        }
+    }
+    class Person{
+
+        String name;    // имя
+        int age;        // возраст
+        void displayInfo(){
+            System.out.printf("Name: %s \tAge: %d\n", name, age);
+        }
+    }
+
+}
